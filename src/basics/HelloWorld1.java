15bd2bdb0a9b5a86fd54635599907dbeede86d2d
Dabe16 <112491454+Dabe16@users.noreply.github.com>
2022-09-03 14:11:27 +0800

    Update HelloWorld1.java

--- src/basics/HelloWorld1.java
+++ src/basics/HelloWorld1.java
@@ -5,7 +5,7 @@ public class HelloWorld1 {
 	public static void main(String[] args) {
 		system.out.println("Hello World");
 		system.out.println("I'm adding code to my project");
-		system.out.println("i'm adding code to my eclipse);
+		system.out.println("i'm adding code to my eclipse");
 
 	}
 
