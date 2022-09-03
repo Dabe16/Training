3a0da7f3917d99c8af0f4bf245814c1c10c407bf
Dabe16 <112491454+Dabe16@users.noreply.github.com>
2022-09-03 14:11:11 +0800

    Update HelloWorld1.java

--- src/basics/HelloWorld1.java
+++ src/basics/HelloWorld1.java
@@ -3,7 +3,9 @@ package basics;
 public class HelloWorld1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		system.out.println("Hello World");
+		system.out.println("I'm adding code to my project");
+		system.out.println("i'm adding code to my eclipse);
 
 	}
 
